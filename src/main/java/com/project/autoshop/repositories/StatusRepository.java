f1932f37508a28d4ccc634bb0c4c13b6ff2ca4b9
ismael101 <ismaelomermohamed@gmail.com>
2022-05-01 09:16:14 -0500

    create status repo

--- src/main/java/com/project/autoshop/repositories/StatusRepository.java
+++ src/main/java/com/project/autoshop/repositories/StatusRepository.java
@@ -3,10 +3,13 @@ package com.project.autoshop.repositories;
 import com.project.autoshop.models.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StatusRepository extends JpaRepository<Status, Integer> {
+    //query for fetching status by work
     @Query("SELECT s FROM Status WHERE s.work = ?1")
-    Optional findStatusByWork(Integer id);
+    Optional<Status> findStatusByWork(Integer id);
 }
